961d3cee3a4330bf95749676f5e7050f997f3da6
arsen novikov <arsennovikov@Air-de-arsen.lan>
2026-03-18 15:57:41 +0100

    fix server

--- server.js
+++ server.js
@@ -5,15 +5,23 @@ app.use(express.json());
 app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/dashboard', (req, res) => {
   res.sendFile(__dirname + '/dashboard.html');
 });
 
+app.get('/form', (req, res) => {
+  res.sendFile(__dirname + '/form.html');
+});
+
 app.post('/order', (req, res) => {
-  console.log(req.body);
+  console.log("Новый заказ:", req.body);
   res.json({ success: true });
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log('Server running on ' + PORT);
